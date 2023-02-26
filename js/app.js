98aadc5dd51c2cba0777f62df72c7f36dec0d0fb
Idakronborgn <55064627+Idakronborgn@users.noreply.github.com>
2023-02-26 15:51:14 +0100

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,224 @@
+import { navigateTo, initRouter } from "./router.js";
+
+// =========== Movie SPA functionality =========== //
+
+let _movies = [];
+let _selectedMovieId;
+
+async function init() {
+    initRouter();
+    await getMovies();
+    await getCategories();
+}
+init();
+
+// fetch all movies from WP
+async function getMovies() {
+    let response = await fetch("https://feed.entertainment.tv.theplatform.eu/f/jGxigC/bb-all-pas/106608168011?form=json");
+    let data = await response.json();
+    _movies = data;
+    appendMovies(data, "#movies-container");
+    showLoader(false);
+}
+
+// append movies to the DOM
+function appendMovies(movies, container) {
+    let htmlTemplate = "";
+    for (let movie of movies) {
+        htmlTemplate += /*html*/ `
+        <article onclick="showDetailView('${movie.id}')">
+            <h2>${movie.title.rendered} (${movie.acf.year})</h2>
+            <img src="${getFeaturedImageUrl(movie)}">
+            <p>${movie.excerpt.rendered}</p>
+        </article>
+    `;
+    }
+    document.querySelector(container).innerHTML = htmlTemplate;
+}
+
+// appends posts for each cagtegory on the search page
+function appendPostsByCategories(categories) {
+    let html = "";
+    // loop through all categories: Action, Adventure, Animation, Comedy, Drama Fantasy, Horror, etc.
+    for (const category of categories) {
+        // creating html for the catgegory title (<h2>Actions</h2>) - headers on search page
+        html += /*html*/ `<h2>${category.name}</h2>`;
+        // looping through all movies
+        for (const movie of _movies) {
+            // if the movie has the id of the given category, ex id of Action
+            if (movie.categories.includes(category.id)) {
+                // creating html for the movie
+                html += /*html*/ `
+                    <article onclick="showDetailView('${movie.id}')">
+                        <h2>${movie.title.rendered} (${movie.acf.year})</h2>
+                        <img src="${getFeaturedImageUrl(movie)}">
+                        <p>${movie.excerpt.rendered}</p>
+                    </article>
+                `;
+            }
+        }
+    }
+    // when looped through all categories and created html for all matching movies, add html to search page (#categories-container)
+    document.querySelector("#categories-container").innerHTML = html;
+}
+
+// search functionality
+function search(value) {
+    // show or hide results (search container)
+    if (value) {
+        document.querySelector("#search-container").classList.remove("hide");
+        document.querySelector("#categories-container").classList.add("hide");
+    } else {
+        document.querySelector("#search-container").classList.add("hide");
+        document.querySelector("#categories-container").classList.remove("hide");
+    }
+
+    let searchQuery = value.toLowerCase();
+    let filteredMovies = [];
+    for (let movie of _movies) {
+        let title = movie.title.rendered.toLowerCase();
+        if (title.includes(searchQuery)) {
+            filteredMovies.push(movie);
+        }
+    }
+    appendMovies(filteredMovies, "#movies-search-container");
+}
+
+// fetch all genres / categories from WP
+async function getCategories() {
+    let response = await fetch("https://movie-api.cederdorff.com/wp-json/wp/v2/categories");
+    let data = await response.json();
+    console.log(data);
+    appendCategories(data);
+    appendPostsByCategories(data);
+}
+
+// append all genres as select options (dropdown)
+function appendCategories(categories) {
+    let htmlTemplate = "";
+    for (let category of categories) {
+        htmlTemplate += /*html*/ `
+        <option value="${category.id}">${category.name} (${category.count})</option>
+    `;
+    }
+    document.querySelector("#select-category").innerHTML += htmlTemplate;
+}
+
+// category selected event - fetch movies by selected category
+async function categorySelected(categoryId) {
+    if (categoryId) {
+        showLoader(true);
+        let response = await fetch(
+            `https://movie-api.cederdorff.com/wp-json/wp/v2/posts?_embed&categories=${categoryId}`
+        );
+        let data = await response.json();
+        appendMoviesByCategory(data);
+        showLoader(false);
+    }
+}
+
+// append movies by genre
+function appendMoviesByCategory(moviesByCategory) {
+    let htmlTemplate = "";
+    for (let movie of moviesByCategory) {
+        htmlTemplate += /*html*/ `
+        <article onclick="showDetailView('${movie.id}')">
+            <h2>${movie.title.rendered} (${movie.acf.year})</h2>
+            <img src="${getFeaturedImageUrl(movie)}">
+            <p>${movie.excerpt.rendered}</p>
+        </article>
+    `;
+    }
+    // if no movies, display feedback to the user
+    if (moviesByCategory.length === 0) {
+        htmlTemplate = /*html*/ `
+      <p>No Movies</p>
+    `;
+    }
+    document.querySelector("#movies-by-category-container").innerHTML = htmlTemplate;
+}
+
+// get the featured image url
+function getFeaturedImageUrl(post) {
+    let imageUrl = "";
+    if (post._embedded["wp:featuredmedia"]) {
+        imageUrl = post._embedded["wp:featuredmedia"][0].source_url;
+    }
+    return imageUrl;
+}
+
+function showDetailView(id) {
+    const movie = _movies.find(movie => movie.id == id);
+    document.querySelector("#detailView h2").innerHTML = movie.title.rendered;
+    document.querySelector("#detailViewContainer").innerHTML = /*html*/ `
+        <img src="${getFeaturedImageUrl(movie)}">
+        <article>
+            <h1>${movie.title.rendered}</h1>
+            <h2>${movie.acf.year}</h2>
+            <p>${movie.content.rendered}</p>
+            <iframe src="${
+                movie.acf.trailer
+            }" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+        </article>
+    `;
+    navigateTo("#/detailView");
+}
+
+if (!_selectedMovieId && location.hash == "#/detailView") {
+    navigateTo("#/");
+}
+
+// =========== Loader functionality =========== //
+
+function showLoader(show = true) {
+    let loader = document.querySelector("#loader");
+    if (show) {
+        loader.classList.remove("hide");
+    } else {
+        loader.classList.add("hide");
+    }
+}
+
+// =========== Attach Events =========== //
+window.showDetailView = id => showDetailView(id);
+window.categorySelected = categoryId => categorySelected(categoryId);
+window.search = value => search(value);
+
+
+
+
+// =========== min egen kode der virkede i en anden mappe =========== //
+
+// const searchBtn = document.getElementById('search-btn');
+// const movieList = document.getElementById('movie');
+// const movieDetailsContent = document.getElementById('.movie-details-content');
+// const movieCloseBtn = document.getElementById('movie-close-btn');
+
+// searchBtn.addEventListener('click', getMovieList);
+
+// function getMovieList(){
+//   let searchInputTxt = document.getElementById
+//   ('searchInput');
+//   fetch(GET = 'https://feed.entertainment.tv.theplatform.eu/f/jGxigC/bb-all-pas?form=json&range=1-20&byTags=genre:action&byYear=2017&byProgramType=movie')
+//   .then(Response => Response.json())
+//   .then(data => {
+//     let html = "";
+//     if(data.entries) {
+//         data.entries.forEach(movie => {
+//           html +=`
+//           <div class="movie-item" data-id="${movie.id}" data-id="${movie.plprogram$thumbnails}">
+//           <div class="movie-name">
+//             <h3>${movie.title}</h3>
+//             <div class="movie-img">
+//             <img src="${movie.plprogram$url}"> //virker ikke med img
+//             </div>
+//             <p>${movie.description}</p>
+//             <a href="#" class="recipe-btn">Se mere!</a>
+//           </div>
+//         </div>
+//           `;
+//         });
+//     }
+//     movieList.innerHTML = html;
+//   });
+// }
